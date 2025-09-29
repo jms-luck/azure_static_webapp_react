6ad6dd65c2fb8ba12667237721a21a0cc2bf40d0
jms-luck <meenachisundaresan24@gmail.com>
2025-09-29 22:04:36 +0530

    intial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Meena";
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Website</h1>
+        <div className="name-display">
+          <h2>Hello, my name is {name}!</h2>
+          <p>Nice to meet you!</p>
+        </div>
       </header>
     </div>
   );
